838428731cbfd91bf3770d82273746cfa8e0cb3f
Frankyboy07 <skylinefranc@gmail.com>
2024-06-08 14:08:00 -0500

    timings changed

--- main.py
+++ main.py
@@ -29,9 +29,9 @@ def start_timer():
     global reps
     reps += 1
     print(reps)
-    work_sec = WORK_MIN * 10
-    short_break_sec = SHORT_BREAK_MIN * 10
-    long_break_sec = LONG_BREAK_MIN * 10
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
     if reps % 8 == 0:
         count_down(long_break_sec)
         title.config(fg=RED, text="Break")
